1a4d74eee9e9a1cbb9d5d3c226a5f87db23063e8
omarwebdeve <omarf977865@gmail.com>
2023-03-14 07:29:49 +0600

    start

---
+++ src/pages/blog/[blogid].tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+const blogid = () => {
+
+    const router = useRouter()
+    const id = router.query.blogid
+
+    return (
+
+        <div>
+            <h1>This is a dynamic page {id}</h1>             
+        </div>
+    );
+};
+
+export default blogid;
---
+++ src/pages/blog/blog.tsx
@@ -0,0 +1,9 @@
+const blog = () => {
+    return (
+        <div>
+            <h1>This is  a blog page </h1>    
+        </div>
+    );
+};
+
+export default blog;
